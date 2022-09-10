46636bc88d34042645bdafd2897d2440582a6172
valkiriy <valkiriy@li.ru>
2022-09-10 12:03:15 +0300

    фикс правок 2

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -14,14 +14,15 @@ function App() {
 
     React.useEffect(() => {
         const loadData = async () => {
-            try {
-                await fetch(API_URL)
-                    .then(res => res.json())
-                    .then(data => setIngredients(data.data))
-                    .catch(err => console.log(err))
-            }catch (e) {
-                console.log(e)
-            }
+            await fetch(API_URL)
+                .then((res) => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                    return Promise.reject(`Ошибка ${res.status}`);
+                })
+                .then(data => setIngredients(data.data))
+                .catch(err => console.log(err))
         }
         loadData();
     }, [])
